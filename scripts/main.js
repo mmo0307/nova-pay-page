e36fdd3d961b0b9ae00095631f43e421642f49af
misamisin <m.misha0307@gmail.com>
2024-07-01 13:11:31 +0300

    add form & styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
-function onShowDropDownClick() {
+const dropdownDetailsBlock = document.querySelector('.details-block');
+const dropdownImg = document.querySelector('.details-block img');
+const dropdownContent = document.getElementById('drop-down-content');
 
-}
+dropdownDetailsBlock.addEventListener('click', function() {
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
 
-function onHideDropDownClick() {
+        dropdownImg.classList.remove('rotate');
+    } else {
+        dropdownContent.style.display = 'block';
 
-}
+        dropdownImg.classList.add('rotate');
+    }
+});
